715cfdc09960ec28e2113a204369bcaac9851b32
The-Anton <shekhar59324@gmail.com>
2021-03-28 14:16:13 +0530

    created ground water fragment ui and connected live data

--- app/src/main/java/com/solvabit/climate/fragment/StatsFragments/GroundWaterStatsFragment.kt
+++ app/src/main/java/com/solvabit/climate/fragment/StatsFragments/GroundWaterStatsFragment.kt
@@ -5,56 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import com.solvabit.climate.R
+import com.solvabit.climate.databinding.FragmentForestDensityStatsBinding
+import com.solvabit.climate.databinding.FragmentGroundWaterStatsBinding
+import com.solvabit.climate.fragment.Dashboard
+import kotlinx.android.synthetic.main.fragment_ground_water_stats.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GroundWaterStatsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class GroundWaterStatsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentGroundWaterStatsBinding
+    val localuser = Dashboard.localuser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ground_water_stats, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_ground_water_stats, container, false
+        )
+
+        addData()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GroundWaterStatsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GroundWaterStatsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun addData() {
+        circularloader(
+            localuser.groundWaterData[11]?.toFloat()
+                ?: 0f, 300f, binding.circularProgressBarGroundWaterStageStats
+        )
+
+
+        binding.apply {
+            this.groundwaterstage.text = localuser.groundWaterData[11].toString()
+            this.annualDomesticAllocationBy2025.text = localuser.groundWaterData[0].toString()
+            this.currentAllocationForUse.text = localuser.groundWaterData[3].toString()
+            this.totalAnnualRecharge.text = localuser.groundWaterData[5].toString()
+            this.currentAnnualExtraction.text = localuser.groundWaterData[2].toString()
+            this.totalNaturalDischarge.text = localuser.groundWaterData[12].toString()
+            this.netAvaibilityForFuture.text = localuser.groundWaterData[6].toString()
+            this.rainfallMonsoonRecharge.text = localuser.groundWaterData[9].toString()
+            this.rainfallNonMonsoonRecharge.text = localuser.groundWaterData[10].toString()
+
+        }
+
+
+
+
+    }
+
+
+
+    fun circularloader(data: Float, max: Float, circularProgressBar: CircularProgressBar) {
+        circularProgressBar.apply {
+            setProgressWithAnimation(data, 3000) // =1s
+            progressMax = max
+            roundBorder = true
+            startAngle = 180f
+        }
     }
 }
