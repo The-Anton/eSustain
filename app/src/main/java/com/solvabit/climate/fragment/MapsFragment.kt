c2933129a2f7708d4292a0ba00cee01c197fe3c6
The-Fuse <rohitkumaryadav141002@gmail.com>
2021-04-03 02:24:00 +0530

    Maps added

--- app/src/main/java/com/solvabit/climate/fragment/MapsFragment.kt
+++ app/src/main/java/com/solvabit/climate/fragment/MapsFragment.kt
@@ -1,28 +1,38 @@
 package com.solvabit.climate.fragment
 
-import androidx.fragment.app.Fragment
-
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.os.TokenWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.*
-
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
+import com.google.android.gms.maps.model.*
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.solvabit.climate.PostItem
 import com.solvabit.climate.R
 import com.solvabit.climate.dataModel.PlantedTrees
-import com.solvabit.climate.dataModel.Post
+
 
 class MapsFragment : Fragment() {
 
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+    }
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -42,7 +52,12 @@ class MapsFragment : Fragment() {
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(australiaBounds, 0))
         googleMap.uiSettings.isZoomControlsEnabled=true
-        googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(requireContext(),R.raw.style_json))
+        googleMap.setMapStyle(
+            MapStyleOptions.loadRawResourceStyle(
+                requireContext(),
+                R.raw.style_json
+            )
+        )
         val ref = FirebaseDatabase.getInstance().getReference("/plantedTrees")
         ref.addChildEventListener(object : ChildEventListener {
 
@@ -54,16 +69,34 @@ class MapsFragment : Fragment() {
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
                 val data = p0.getValue(PlantedTrees::class.java)
                 val location = data?.lon?.let { LatLng(data.lat, it) }
-                googleMap.addMarker(location?.let { MarkerOptions().position(it).title("Rohit Kumar Planted tree on 02/03/21") })
+                googleMap.addMarker(location?.let {
+                    MarkerOptions().position(it).title("Rohit Kumar Planted tree on 02/03/21").snippet("Population: 4,137,400")
+                        .icon(bitmapDescriptorFromVector(requireContext(),R.drawable.marker))
+                })
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
             }
         })
 
+        googleMap.setOnMarkerClickListener(OnMarkerClickListener { marker ->
+            val position = marker.position
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Location of the tree planted")
+            builder.setMessage("Position is $position")
+            builder.setIcon(android.R.drawable.ic_dialog_map)
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(true)
+            alertDialog.show()
+            false
+        })
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
