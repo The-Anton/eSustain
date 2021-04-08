bc196981afb6cf923afdf88ebd8d78a47cb05d4c
Chandresh Singh <chaharchandresh@gmail.com>
2021-04-08 10:39:02 +0530

    Designed beginning tasks layout | added logic for grayscale

--- app/src/main/java/com/solvabit/climate/fragment/TaskFragment.kt
+++ app/src/main/java/com/solvabit/climate/fragment/TaskFragment.kt
@@ -1,58 +1,46 @@
 package com.solvabit.climate.fragment
 
+import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.solvabit.climate.R
+import com.solvabit.climate.databinding.FragmentTaskBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TaskFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentTaskBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_task, container, false)
+
+
+        val matrix = floatArrayOf(
+                0.33f, 0.33f, 0.33f, 0f, 0f,
+                0.33f, 0.33f, 0.33f, 0f, 0f,
+                0.33f, 0.33f, 0.33f, 0f, 0f,
+                0f, 0f, 0f, 0.5f, 0f)
+        binding.conserveElectricityImageViewTasks.colorFilter = ColorMatrixColorFilter(matrix)
+//        binding.seminarImageViewTasks.colorFilter = ColorMatrixColorFilter(matrix)
+        binding.shareReportImageViewTasks.colorFilter = ColorMatrixColorFilter(matrix)
+
+        binding.greenTickAnimationConserveElectricity.visibility = View.GONE
+        binding.greenTickAnimationSeminar.visibility = View.GONE
+        binding.greenTickAnimationShareReport.visibility = View.GONE
+
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                TaskFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
 }
